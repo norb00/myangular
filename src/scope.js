156790db1434f1566576a7e2e2cdd6282b598b45
Bagi Norbert <bagi.norbert@gmail.com>
2016-09-29 00:08:03 +0200

    Watch, Digest changes

--- src/scope.js
+++ src/scope.js
@@ -5,17 +5,30 @@ function Scope() {
     this.$$watchers = [];
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+function initWatchVal() { }
+
+Scope.prototype.$watch = function (watchFn, listenerFn) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn || function () { },
+        last: initWatchVal
     };
     this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function() {
-    _.forEach(this.$$watchers, function(watcher) {
-        watcher.listenerFn();
+Scope.prototype.$digest = function () {
+    var self = this;
+    var newValue, oldValue;
+    _.forEach(this.$$watchers, function (watcher) {
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (newValue !== oldValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue,
+                (oldValue === initWatchVal ? newValue : oldValue),
+                self);
+        }
+
     });
 };
 
