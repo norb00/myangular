6761336855c2dfaea358881ca3290c0b8d8f4a27
Norbert Bagi <norbert_bagi@epam.com>
2017-04-03 16:29:28 +0200

    Parse 1

---
+++ src/parse.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//page 189
+
+function parse(expr) {
+    var lexer = new Lexer();
+    var parser = new Parser(lexer);
+    return parser.parse(expr);
+}
+
+function Lexer() {}
+
+Lexer.prototype.lex = function (text) {
+    this.text = text;
+    this.index = 0;
+    this.ch = undefined;
+    this.tokens = [];
+
+    while(this.index < this.text.length) {
+        this.ch = this.text.charAt(this.index);
+    }
+
+    return this.tokens;
+
+};
+
+function AST(lexer) {
+    this.lexer = lexer;
+}
+AST.prototype.ast = function (text) {
+    this.tokens = this.lexer.lex(text);
+    // AST building will be done here
+};
+
+function ASTCompiler(astBuilder) {
+    this.astBuilder = astBuilder;
+}
+ASTCompiler.prototype.compile = function (text) {
+    var ast = this.astBuilder.ast(text);
+    // AST compilation will be done here
+};
+
+function Parser(lexer) {
+    this.lexer = lexer;
+    this.ast = new AST(this.lexer);
+    this.astCompiler = new ASTCompiler(this.ast);
+}
+Parser.prototype.parse = function (text) {
+    return this.astCompiler.compile(text);
+};
+
+module.exports = parse;
